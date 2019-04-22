198e7e521f57da2cb834148dd101257cc0bd82b8
Belov Pavel <belovpavel@protonmail.com>
2019-04-22 20:29:30 +0300

    hw#3 tv-maze-show-cdu

---
+++ src/components/Show/Show.js
@@ -0,0 +1,56 @@
+import React, { PureComponent, Fragment } from 'react';
+import { getShowInfo } from '../../api';
+import './Show.css';
+
+class Show extends PureComponent {
+  state = {
+    showId: '',
+    data: {}
+  };
+
+  innerHtml = html => {
+    return {
+      toHtml: html
+    };
+  };
+
+  componentDidUpdate = async prevProps => {
+    const { showId } = this.props;
+
+    if (showId !== prevProps.showId) {
+      let data = await getShowInfo(showId);
+
+      this.setState({
+        showId: showId,
+        data: data
+      });
+    }
+  };
+
+  render() {
+    const { showId, data } = this.state;
+    const { name, image, genres, summary } = data;
+
+    return (
+      <div className="show">
+        {showId ? (
+          <Fragment>
+            <img className="show-image" src={image.medium} alt={name} />
+            <h2 className="show-label t-show-name">{name}</h2>
+            <p className="show-text t-show-genre">
+              <b>Жанр: </b> {genres.join(', ')}
+            </p>
+            <p
+              className="show-text t-show-summary"
+              dangerouslySetInnerHTML={this.innerHtml(summary)}
+            />
+          </Fragment>
+        ) : (
+          <p className="show-information t-show-info">Шоу не выбрано</p>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Show;
