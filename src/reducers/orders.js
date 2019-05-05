e31b75dd5aea4b58f0fc0785b63e79d16208bab5
Belov Pavel <belovpavel@protonmail.com>
2019-05-05 13:40:12 +0300

    hw#7

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -9,6 +9,84 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER: {
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          recipe: action.payload.recipe,
+          ingredients: [],
+          position: 'clients'
+        }
+      ];
+    }
+
+    case MOVE_ORDER_NEXT: {
+      return state.map(order => {
+        if (order.id !== action.payload) {
+          return order;
+        }
+
+        switch (order.position) {
+          case 'clients':
+            return { ...order, position: 'conveyor_1' };
+          case 'conveyor_1':
+            return { ...order, position: 'conveyor_2' };
+          case 'conveyor_2':
+            return { ...order, position: 'conveyor_3' };
+          case 'conveyor_3':
+            return { ...order, position: 'conveyor_4' };
+          case 'conveyor_4':
+            return order.recipe.every(ingredient =>
+              order.ingredients.includes(ingredient)
+            )
+              ? { ...order, position: 'finish' }
+              : order;
+          default:
+            return order;
+        }
+      });
+    }
+
+    case MOVE_ORDER_BACK: {
+      return state.map(order => {
+        if (order.id !== action.payload) {
+          return order;
+        }
+
+        switch (order.position) {
+          case 'clients':
+            return { ...order, position: 'conveyor_1' };
+          case 'conveyor_2':
+            return { ...order, position: 'conveyor_1' };
+          case 'conveyor_3':
+            return { ...order, position: 'conveyor_2' };
+          case 'conveyor_4':
+            return { ...order, position: 'conveyor_3' };
+          default:
+            return order;
+        }
+      });
+    }
+
+    case ADD_INGREDIENT: {
+      return state.map(order => {
+        const { from, ingredient } = action.payload;
+
+        if (order.position !== from) {
+          return order;
+        }
+
+        return order.recipe.includes(ingredient) &&
+          !order.ingredients.includes(ingredient)
+          ? {
+              ...order,
+              ingredients: [...order.ingredients]
+            }
+          : order;
+      });
+    }
+
     default:
       return state;
   }
